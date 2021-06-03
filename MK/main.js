2f8a585668883b6307ce67218d7f435786522a70
Web-Marat <84325913+Web-Marat@users.noreply.github.com>
2021-06-03 19:58:36 +0300

    HomeWork 2

--- MK/main.js
+++ MK/main.js
@@ -1,3 +1,80 @@
 "use strict";
+const weapon = ['Knight', 'Shuriken', 'Sword', 'Katana', 'Gun'];
+const name1 = 'Scorpion'
+const name2 = 'Kitana'
+const name3 = 'Liukang'
+const name4 = 'Sonya'
+const name5 = 'Subzero'
 
-alert('Fight...');
+
+const player1 = {
+	name: name1,
+	hp: 100,
+	img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+	weapon: (weapon[0]),
+	attack: function() {
+		console.log(name1 + ' fight ' + name2 + ' - Fight...');
+	}
+};
+player1.attack();
+console.log(player1);
+
+
+const player2 = {
+	name: name2,
+	hp: 100,
+	img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+	weapon: (weapon[1]),
+	attack: function() {
+		console.log(name2 + ' fight ' + name1 + ' - Fight...');
+	}
+};
+player2.attack();
+console.log(player2);
+
+
+
+
+function createPlayer(player, name, hp){
+	const $player1 = document.createElement('div');
+	$player1.classList.add('player');
+
+
+	const $progressbar = document.createElement('div');
+	$progressbar.classList.add('progressbar');
+
+	const $life = document.createElement('div');
+	$life.classList.add('life');
+	$life.classList.add('hp');
+	$life.style.width = '100%';
+
+
+	const $name = document.createElement('div');
+	$name.classList.add('name');
+	$name.innerText = 'Scorpion';
+
+
+	const $character = document.createElement('div');
+	$character.classList.add('character');
+
+	const $img = document.createElement('img');
+	$img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+
+
+
+
+	const $root = document.querySelector('.arenas');
+	$root.appendChild($player1);
+
+	$player1.appendChild($progressbar);
+
+	$progressbar.appendChild($life);
+	$progressbar.appendChild($name);
+
+	$player1.appendChild($character);
+	$character.appendChild($img);	
+}
+
+createPlayer ('player1' , 'Scorpion' , 50);
+
+
